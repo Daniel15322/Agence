efbc5dbda945c8f4d601543aa4b8ac3f98e1bad8
Daniel15322 <dc8815322@gmail.com>
2024-07-12 23:32:11 +0200

    Create Evaluations.php

---
+++ Evaluations.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Temoignage</title>
+</head>
+<section class="Temoignage" id="Temoignage">
+    <div class="titre blanc">
+        <h2 class="titre-texte"><span>Q</span>ue disent nos  <span>C</span>lients</h2>
+     </div>
+     <div class="contenu">
+        <div class="box">
+            <div class="imbox">
+                <img src="./image/Screenshot_20230906-084003.jpg" alt="">
+            </div>
+            <div class="text">
+                <p>J'ai été satisfait par le service qui m'a été rendu par l'agence, je viens de recevoir mes colis! Merci beaucoup ELAIZ</p>
+                <h3>Chris BAHARI</h3>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imbox">
+                <img src="./image/IMG-20231202-WA0008.jpg" alt="">
+            </div>
+            <div class="text">
+                <p>Je suis très heureux, car je viens de recevoir mes colis dans le temps.</p>
+                <h3>Edouard KABAMBA</h3>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imbox">
+                <img src="./image/IMG-20240213-WA0048.jpg" alt="">
+            </div>
+            <div class="text">
+                <p>Merci beaucoup ELAIZ, mes colis sont bien arrivés.</p>
+                <h3>Fifi MASENGO</h3>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imbox">
+                <img src="./image/W99_9506.jpg" alt="">
+            </div>
+            <div class="text">
+                <p>Meme si ça a mis du temps, merci quand meme à vous ELAIZ! Je suis très satisfaite.</p>
+                <h3>Julia BAMS</h3>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imbox">
+                <img src="./image/Screenshot_20230904-145220.jpg" alt="">
+            </div>
+            <div class="text">
+                <p>Je remercie tous les agents de l'agence, merci pour le service rendu.</p>
+                <h3>Mosche MAMPUYA</h3>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imbox">
+                <img src="./image/IMG-20231206-WA0008.jpg" alt="">
+            </div>
+            <div class="text">
+                <p>J'ai beaucoup douté de vous avant de vous connaitre, mais j'ai été satisfaite.</p>
+                <h3>NTUMBA Mireille</h3>
+            </div>
+        </div>
+     </div>
+</section>
+<a href="index.html"><input type="button" value="Accueil" name="Accueil"></a>
+<a href="services.html"><input type="button" value="Service" name="Service"></a>
+<a href="contact.php"><input type="button" value="Secretariat" name="Secretariat"></a>
+<a href="Facultés.html"><input type="button" value="Agences" name="Agences"></a>
+<div class="copyright">
+    <p> © copyright 2024 <a href="#">Kings DANIEL</a> Tout droit reservé</p>
+</div>
+</body>
+</html>
