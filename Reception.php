342dcb24b3cf302382f8db0962ec7eca340519d9
Daniel15322 <dc8815322@gmail.com>
2024-07-12 23:17:05 +0200

    Create Reception.php

---
+++ Reception.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Apparitorat</title>
+</head>
+<section class="Apparitorat" id="Apparitorat">
+    <h1 class="title">A PROPOS</h1>
+    <p class="sub-title">
+        <br>  
+        Bienvenue chez ELAIZ TRADING SERVICES, basée en Afrique du Sud! Nous sommes une agence de frets, nous assurons le transport des colis.
+                Ces véhicules passent de l'Afrique du sud au Congo par divers pays notamment le Zimbabwe et la Zambie. 
+                Dans la ville de Lubumbashi, l'agence est située à la route karavia réf cercle hippique à proximité de l école Mwapusukeni. 
+ Nous faisons le transport des marchandises par des camions poids-lourds communément appelés TrucKs, à Lubumbashi se trouve un bureau de réception des clients et le dépôt de livraison des colis.                 En Afrique du sud se trouve le directeur général 
+Monsieur  Papy NTUMBUDILA ainsi un certains nombres d employés (secrétaire, chef du dépôt Monsieur  Philipe  NTUMBUDILA, d'un chargé des logistiques Arsène TSHIBANGU, chargeur du véhicules et les chauffeurs).
+<br>Les colis sont taxés par kilo(2$ par kilo) ou par volume. </p>
+    </p>
+    <br>
+    <p class="sub-title">Nous sommes ouverts du lundi au samedi de 8H00 à 15H30<br><b>SOYEZ LES BIENVENUS</b></p>
+    <div class="list-Apparitorat">
+        <div class="box">
+            <h1><a href="Formulaire.php"> RESERVATION</a></h1>
+            <p>Cliquez ici pour réserver votre place</p>
+        </div>
+</section>
+<div class="copyright">
+    <p> © copyright 2024 <a href="#">Kings DANIEL</a> Tout droit reservé</p>
+</div>
+</body>
+</html>
